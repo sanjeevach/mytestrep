12516fcbf625e5c3c49aa5d368d5b6f2da32dd7e
sanjeevach <93425920+sanjeevach@users.noreply.github.com>
2021-11-06 16:22:29 +0530

    Done my changes on branch myerstbrnch

--- test.c
+++ test.c
@@ -5,7 +5,8 @@ int main(void)
   printf("Welcome to Hello Git World !!\n");
   printf("Test changes 1 added\n");
   printf("Test changes 2 added\n");
-  printf("Test changes to the new brach myerstbrnch \n");
+  printf("Test changes to the new branch myerstbrnch \n");
+  printf("Done my changes on branch myerstbrnch \n");
   
   return 0;
 }
